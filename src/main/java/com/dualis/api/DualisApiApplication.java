35520e9abe23f3049f7cc1803a514286f5e00cd9
jorgeLinares <jorgelinaresrrs306@hotmail.com>
2026-07-25 12:24:38 -0500

    first commit

---
+++ src/main/java/com/dualis/api/DualisApiApplication.java
@@ -0,0 +1,13 @@
+package com.dualis.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DualisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DualisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dualis/api/DualisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dualis.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DualisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
